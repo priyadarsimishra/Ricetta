151de4e7249978ac0860ab33504c0b54349e527d
Justin Lopato <justin@lopato.org>
2022-03-23 22:26:05 +0000

    main page ui

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,28 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from 'next/link';
 import { Box } from '@chakra-ui/react'
+import { Badge } from '@chakra-ui/react'
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
-		<Box bg="black" color="#D6C8DA" w="100%" h="73px">
+		<div>
 			<Head>
 				<title>Ricetta</title>
 			</Head>
-			<h1>Ricetta</h1>
-			<Link href="signup">
-				<a>sign up</a>
-			</Link>
-			<Link href="login">
-				<a>sign in</a>
-			</Link>
-		</Box>
+			<Box bg="#857393" color="white" w="100%" h="73px">
+				<Link href="signup">
+					<a>Sign Up </a>
+				</Link>
+				<Badge display="flex" borderRadius="45px" bg="#3B1A4A" color="white" h="90px" w="300px" textAlign="center" justifyContent="center">
+					<Link href="">
+						<a>Ricetta</a>
+					</Link>
+				</Badge>
+				<Link href="login">
+					<a>Sign In</a>
+				</Link>
+			</Box>
+		</div>
 	);
 }
