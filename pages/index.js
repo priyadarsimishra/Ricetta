595df574c8819601ac2eeeda2a1827e63e2149dd
Justin Lopato <justin@lopato.org>
2022-03-23 22:12:12 +0000

    main page ui

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from 'next/link';
+import { Box } from '@chakra-ui/react'
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Ricetta</title>
-      </Head>
-
-      <h1>Ricetta</h1>
-    </div>
-  );
+	return (
+		<Box bg="black" color="white" w="100%" h="73px">
+			<Head>
+				<title>Ricetta</title>
+			</Head>
+			<h1>Ricetta</h1>
+			<Link href="signup">
+				<a>sign up</a>
+			</Link>
+			<Link href="login">
+				<a>sign in</a>
+			</Link>
+		</Box>
+	);
 }
